cd57bf99eff39fdd270888406290700e200cafa3
71M073J <ciglarictimotej@gmail.com>
2023-03-01 15:17:41 +0100

    some progress

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def lucaskanade(im1, im2, N):
     # im2 − sec ond image mat rix ( g r a y s c a l e )
     # n − s i z e o f the nei ghb o rh o od (N x N)
     # TODO : the a l g o ri t h m
-    im1 = im1.astype(np.float32)
-    im2 = im2.astype(np.float32)
+    im1 = im1.astype(np.float32) / np.max(im1)
+    im2 = im2.astype(np.float32) /np.max(im2)
     sigma = 1
     im1 = utils.gausssmooth(im1, sigma)
     im2 = utils.gausssmooth(im2, sigma)
@@ -20,6 +20,12 @@ def lucaskanade(im1, im2, N):
     dt = im2 - im1
     dx, dy = utils.gaussderiv(im1, sigma)
     neighbourhood_size = N
+    #TODO PYRAMIDAL
+
+    #TODO HARRIS
+    #corner = cv2.cornerHarris(dx, neigh, 3, 0.05)
+    #plt.imshow(corner)
+    #plt.show()
 
     sumdxdy = cv2.filter2D(dx * dy, -1, np.ones((neighbourhood_size, neighbourhood_size)))
     sumdxdt = cv2.filter2D(dx * dt, -1, np.ones((neighbourhood_size, neighbourhood_size)))
@@ -36,7 +42,7 @@ def lucaskanade(im1, im2, N):
     return u, v
 
 
-def hornschunck(im1, im2, n_iters, lmbd):
+def hornschunck(im1, im2, n_iters, lmbd, initial=False):
     # im1 − f i r s t image mat rix ( g r a y s c a l e )
     # im2 − sec ond image ma t rix ( g r a y s c a l e )
     # n_i t e r s − number o f i t e r a t i o n s ( t r y s e v e r a l hundred )
@@ -51,15 +57,17 @@ def hornschunck(im1, im2, n_iters, lmbd):
 
 
     sigma = 1
-    #im1 = utils.gausssmooth(im1, sigma)
-    #im2 = utils.gausssmooth(im2, sigma)
+    im1 = utils.gausssmooth(im1, sigma)
+    im2 = utils.gausssmooth(im2, sigma)
     # dt = dt * (dt > 0)
     dt = im2 - im1
     #dt = cv2.filter2D(im2 - im1, -1, np.ones((2,2)) / 4.0)
     dx, dy = utils.gaussderiv(im1, sigma)
-
-    u = np.zeros(im1.shape)
-    v = np.zeros(im1.shape)
+    if not initial:
+        u = np.zeros(im1.shape)
+        v = np.zeros(im1.shape)
+    else:
+        u, v = lucaskanade(im1, im2, 7)
     ld = np.array([[0, 0.25, 0], [0.25, 0, 0.25], [0, 0.25, 0]])
 
     D = lmbd + dx ** 2 + dy ** 2
@@ -79,8 +87,8 @@ def rotate_image(image, angle):
 
 
 for i in range(3,20,2):
-    #i1 = cv2.imread(f"./collision/{'{:08d}'.format(i)}.jpg", cv2.IMREAD_GRAYSCALE)
-    #i2 = cv2.imread(f"./collision/{'{:08d}'.format(i + 1)}.jpg", cv2.IMREAD_GRAYSCALE)
+    i1 = cv2.imread(f"./collision/{'{:08d}'.format(i)}.jpg", cv2.IMREAD_GRAYSCALE)
+    i2 = cv2.imread(f"./collision/{'{:08d}'.format(i + 1)}.jpg", cv2.IMREAD_GRAYSCALE)
     i1 = np.random.randint(0, 255, (200, 200)).astype(np.uint8)
     i1 = (i1.astype(np.float32) / np.max(i1))
     test = rotate_image(i1, 1)
@@ -89,13 +97,13 @@ for i in range(3,20,2):
     #ax[0].imshow(i1)
     #ax[1].imshow(test)
     #plt.show()
-    u, v = lucaskanade(i1, test, i)
-
+    neigh = 17
+    u, v = lucaskanade(i1, test, neigh)
     fig, ax = plt.subplots(2,2)
     utils.show_flow(u, v, ax[0,0])
     utils.show_flow(u, v, ax[1,0], type="angle_magnitude")
 
-    u, v = hornschunck(i1, test, 500, 0.8)
+    u, v = hornschunck(i1, test, 500, 100, initial=True)
     utils.show_flow(u, v, ax[0, 1])
     utils.show_flow(u, v, ax[1, 1], type="angle_magnitude")
 
